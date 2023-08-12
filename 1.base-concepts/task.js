34277195e9098292129d3b5a7e4a7a6d464aa811
GorVit <muteer555@gmail.com>
2023-08-12 19:57:55 +0500

    fresh

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,21 +1,22 @@
 "use strict"
+
 function solveEquation(a, b, c) {
-  let d = Math.pow(b,2)-4*a*c;
+  let d = Math.pow(b, 2) - 4 * a * c;
 
   if (d < 0) {
     return [];
   }
 
   if (d === 0) {
-    console.log(-b/(2*a));
-    return [-b/(2*a)];
+    console.log(-b / (2 * a));
+    return [-b / (2 * a)];
   }
 
   let arr = [];
-   arr.push((-b + Math.sqrt(d) )/(2*a));
-  arr.push((-b - Math.sqrt(d) )/(2*a));
+  arr.push((-b + Math.sqrt(d)) / (2 * a));
+  arr.push((-b - Math.sqrt(d)) / (2 * a));
 
-  
+
   return arr;
 }
 
