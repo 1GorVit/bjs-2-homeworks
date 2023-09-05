9bd12bd4d4d959964af54129dfbe719ea6acdd7a
GorVit <muteer555@gmail.com>
2023-09-05 19:14:34 +0500

    done

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,21 +1,41 @@
 //Задача № 1
 function cachingDecoratorNew(func) {
-  let cache = [];
-  function wrapper(...args) {
-    const hash = md5(args);
-    let objectInCache = cache.find( item => item.hash === hash);
-    if (objectInCache) {
-      return `Из кэша: ${objectInCache.value}`;
+    let cache = [];
+  
+    function wrapper(...args) {
+      const hash = (JSON.stringify(args)); // вычисляем хеш аргументов
+      let objectInCache = cache.find((item) => item.hash === hash); // ищем элемент в кеше по хешу
+  
+      if (objectInCache) { // если элемент найден в кеше
+        console.log("Из кэша: " + objectInCache.value);
+        return "Из кэша: " + objectInCache.value;
+      }
+  
+      let result = func(...args); // вызываем функцию и получаем результат
+      cache.push({ hash, value: result }); // добавляем новый элемент в кеш
+  
+      if (cache.length > 5) { // если количество элементов в кеше больше пяти
+        cache.shift(); // удаляем первый элемент из кеша
+      }
+  
+      console.log("Вычисляем: " + result);
+      return "Вычисляем: " + result;
     }
-    let result = func(...args);
-    cache.push({"hash": hash, "value": result});
-    if (cache.length > 5) {
-      cache.shift();
-    }
-    return `Вычисляем: ${result}`;
+  
+    return wrapper;
   }
-  return wrapper;
-}
+  
+  const addAndMultiply = (a, b, c) => (a + b) * c;
+  const upgraded = cachingDecoratorNew(addAndMultiply);
+  console.log(upgraded(1, 2, 3)); // вычисляем: 9
+  console.log(upgraded(1, 2, 3)); // из кэша: 9
+  console.log(upgraded(2, 2, 3)); // вычисляем: 12
+  console.log(upgraded(3, 2, 3)); // вычисляем: 15
+  console.log(upgraded(4, 2, 3)); // вычисляем: 18
+  console.log(upgraded(5, 2, 3)); // вычисляем: 21
+  console.log(upgraded(6, 2, 3)); // вычисляем: 18 (при этом кеш для 1, 2, 3 уничтожается)
+  console.log(upgraded(1, 2, 3)); // вычисляем: 9 (снова вычисляем, кеша нет)
+  
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
   //let args = [];
