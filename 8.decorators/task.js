9e4a84dae35625b3fcb00edd91dcb75d9779327b
GorVit <muteer555@gmail.com>
2023-09-05 19:25:58 +0500

    done

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,21 +1,28 @@
 //Задача № 1
 function cachingDecoratorNew(func) {
   let cache = [];
+
   function wrapper(...args) {
-    const hash = md5(args);
-    let objectInCache = cache.find( item => item.hash === hash);
+    const hash = md5(args); // получаем правильный хеш c помощью функции md5
+    let objectInCache = cache.find((item) => item.hash === hash); // ищем элемент, хеш которого равен нашему хешу
+    let index = cache.findIndex((arr) => arr.hash === hash);
     if (objectInCache) {
-      return `Из кэша: ${objectInCache.value}`;
+      // если элемент найден
+      console.log(`Из кэша: ${cache[index].result}`); // индекс нам известен, по индексу в массиве лежит объект, как получить нужное значение?
+      return `Из кэша: ${cache[index].result}`;
     }
-    let result = func(...args);
-    cache.push({"hash": hash, "value": result});
+
+    let result = func(...args); // в кеше результата нет — придётся считать
+    cache.push({ hash, result }); // добавляем элемент с правильной структурой
     if (cache.length > 5) {
-      cache.shift();
+      cache.shift(); // если слишком много элементов в кеше, надо удалить самый старый (первый)
     }
-    return `Вычисляем: ${result}`;
+    console.log("Вычисляем: " + result);
+    return "Вычисляем: " + result;
   }
   return wrapper;
 }
+
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
   //let args = [];
